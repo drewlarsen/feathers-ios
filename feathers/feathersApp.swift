11cdc85db73a975df04ead4b1ee2f12ee5c05731
drewlarsen <drewlarsen03@gmail.com>
2025-05-30 08:33:43 -0600

    Initial Commit

---
+++ feathers/feathersApp.swift
@@ -0,0 +1,17 @@
+//
+//  feathersApp.swift
+//  feathers
+//
+//  Created by Drew Larsen on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct feathersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ feathersTests/feathersTests.swift
@@ -0,0 +1,17 @@
+//
+//  feathersTests.swift
+//  feathersTests
+//
+//  Created by Drew Larsen on 5/30/25.
+//
+
+import Testing
+@testable import feathers
+
+struct feathersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
